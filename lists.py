34cfd0942d8b637a4c6831ed1abe40add64a4bd8
unknown <doradomiguel35@gmail.com>
2018-11-21 09:31:56 +0800

    list under construction

---
+++ lists.py
@@ -0,0 +1,32 @@
+def listAdd(array,addTo):
+	array = list(array)
+	array.append(addTo)
+	print(array)
+
+def listRemove(array,delete):
+	array = list(array)
+	array.remove(delete)
+	print(array)
+
+def listInsert(array,insert):
+	array = list(array)
+	array.insert(insert)
+	print(array)
+
+def listPop(array,index):
+	array = list(array)
+	array.pop(index)
+	print(array)
+
+def listLoop(array):
+	for x in array:
+		print(x)
+
+def listWhile(array):
+	x=0
+	while x <= len(array)
+		print(x)
+		x+=1
+
+def listInt(array):
+	 
